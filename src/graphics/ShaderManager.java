4b72b9beadada02503262c90f3fec3ea0530e3de
Simon Rothman <ssrothman@gmail.com>
2016-12-13 10:25:28 -0600

    resolved merge

--- src/graphics/ShaderManager.java
+++ src/graphics/ShaderManager.java
@@ -64,12 +64,11 @@ public class ShaderManager {
 		setUniform1f("waterClarity", Water.WATER_CLARITY);
 		setUniform1f("maxDistortion", Water.MAX_DISTORTION);
 		setUniform4f("waterColour", new Vector4f(Water.WATER_COLOUR, 1f));
-<<<<<<< HEAD
+
 
 		start(particleShader);
 		setUniformMatrix4f("projection", GraphicsManager.camera.projection);
-=======
->>>>>>> master
+
 		stop();
 	}
 
