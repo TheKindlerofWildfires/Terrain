735308d33e95ec99fc88760fb06706be70a4d57f
TheKindlerofWildfires <Mjhock99@gmail.com>
2017-02-03 13:08:09 -0600

    k

--- src/world/Hole.java
+++ src/world/Hole.java
@@ -1,5 +1,9 @@
 package world;
 
 public class Hole {
-
+	//The idea of this class 
+	//is that I make "a lot" of pre fab tunnels for each world
+	//and connect them together like a game of snakes
+	//color them like the correct color for that elevation/biome
+	//and tell the terrain to piss off when I cross through it
 }
