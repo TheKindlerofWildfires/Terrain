2eb01b38cc723a5a9194baaa5ea9999c4cc29fab
TheKindlerofWildfires <Mjhock99@gmail.com>
2017-02-03 13:09:43 -0600

    sf

--- src/world/Hole.java
+++ src/world/Hole.java
@@ -6,4 +6,7 @@ public class Hole {
 	//and connect them together like a game of snakes
 	//color them like the correct color for that elevation/biome
 	//and tell the terrain to piss off when I cross through it
+	
+	//this branch is depricated, find where we did game features and start working from there! Steal these notes and the playing obj
+	//then git rid of this branch
 }
