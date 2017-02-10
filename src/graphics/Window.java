eb43c27c4a3793bdf93bcf969fd8007dd88fbd81
Kindler <Mjhock99@gmail.com>
2017-02-09 18:41:38 -0600

    I can not make the world render again :(

--- src/graphics/Window.java
+++ src/graphics/Window.java
@@ -159,74 +159,75 @@ public class Window implements Runnable {
 	/**
 	 * Code called at the start of the gameloop
 	 */
-	public void init() {
-		loadProperties();
-		randomize();
-		// Initialize glfw
-		if (!glfwInit()) {
-			System.err.println("GLFW init fail");
-		}
-		// make window resizable
-		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-		// set opengl to version 3.2, core profile, forward compatable
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-		// create window
-		window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "WaterGame", NULL, NULL);
-		if (window == NULL) {
-			System.err.println("Could not create window");
-		}
-		// enable keyboard and mouse
-		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-		glfwSetKeyCallback(window, keyCallback = new input.KeyboardInput());
-		glfwSetCursorPosCallback(window, cursorCallback = (GLFWCursorPosCallback) new input.MouseInput());
-		glfwSetScrollCallback(window, scrollCallback = (GLFWScrollCallback) new input.ScrollCallback());
-		glfwSetMouseButtonCallback(window,
-				mouseButtonCallback = (GLFWMouseButtonCallback) new input.MouseButtonCallback());
-
-		// set window pos
-		glfwSetWindowPos(window, 0, 20);
-		// display window
-		glfwMakeContextCurrent(window);
-		glfwShowWindow(window);
-		// init gl
-		GL.createCapabilities();
-		// background colour
-		glClearColor(CLEAR_COLOUR.x, CLEAR_COLOUR.y, CLEAR_COLOUR.z, 1.0f);
-		// enable depth testing and face culling
-		glEnable(GL_DEPTH_TEST);
-		glEnable(GL_CLIP_DISTANCE0);
-
-		IntBuffer width = BufferUtils.createIntBuffer(1);
-		IntBuffer height = BufferUtils.createIntBuffer(1);
-		glfwGetFramebufferSize(Window.window, width, height);
-		windowWidth = width.get(0);
-		windowHeight = height.get(0);
-		// Create GraphicsManager and World
-		world = new World(0, mathRandom.nextInt());
-		graphicsManager = new GraphicsManager();
-		objectManager = new ObjectManager();
-		entityManager = new EntityManager();
-
-		water = new Water();
-		chunkLoader = new ChunkLoader();
-		chunkLoader.setPriority(Thread.MIN_PRIORITY);
-		chunkLoader.start();
-		lava = new Lava();
-		tunnel = new Tunnel();
-		walk = new Walk();
-		reflectionClipPlane = new Vector4f(0, 0, 1, -Chunk.WATERLEVEL + 0.01f);
-		refractionClipPlane = new Vector4f(0, 0, -1, Chunk.WATERLEVEL + 0.01f);
-		renderClipPlane = new Vector4f(0, 0, -1, 10000);
-
-		reflection = new FrameBufferObject(REFLECTION_WIDTH, REFLECTION_HEIGHT, false);
-		refraction = new FrameBufferObject(REFRACTION_WIDTH, REFRACTION_HEIGHT, true);
-
-		Detail.init();
-		GraphicsManager.toggleFog();
+	private void init(){
+	loadProperties();
+	randomize();
+	// Initialize glfw
+	if (!glfwInit()) {
+		System.err.println("GLFW init fail");
 	}
+	// make window resizable
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+	// set opengl to version 3.2, core profile, forward compatable
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	// create window
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "WaterGame", NULL, NULL);
+	if (window == NULL) {
+		System.err.println("Could not create window");
+	}
+	// enable keyboard and mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetKeyCallback(window, keyCallback = new input.KeyboardInput());
+	glfwSetCursorPosCallback(window, cursorCallback = (GLFWCursorPosCallback) new input.MouseInput());
+	glfwSetScrollCallback(window, scrollCallback = (GLFWScrollCallback) new input.ScrollCallback());
+	glfwSetMouseButtonCallback(window, mouseButtonCallback = (GLFWMouseButtonCallback)new input.MouseButtonCallback());
+
+
+	// set window pos
+	glfwSetWindowPos(window, 0, 20);
+	// display window
+	glfwMakeContextCurrent(window);
+	glfwShowWindow(window);
+	// init gl
+	GL.createCapabilities();
+	// background colour
+	glClearColor(CLEAR_COLOUR.x, CLEAR_COLOUR.y, CLEAR_COLOUR.z, 1.0f);
+	// enable depth testing and face culling
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CLIP_DISTANCE0);
+
+	IntBuffer width = BufferUtils.createIntBuffer(1);
+	IntBuffer height = BufferUtils.createIntBuffer(1);
+	glfwGetFramebufferSize(Window.window, width, height);
+	windowWidth = width.get(0);
+	windowHeight = height.get(0);
+	// Create GraphicsManager and World
+	world = new World(0,mathRandom.nextInt());
+	graphicsManager = new GraphicsManager();
+	objectManager = new ObjectManager();
+	entityManager = new EntityManager();
+
+	water = new Water();
+	chunkLoader = new ChunkLoader();
+	chunkLoader.setPriority(Thread.MIN_PRIORITY);
+	chunkLoader.start();
+	lava = new Lava();
+	reflectionClipPlane = new Vector4f(0, 0, 1, -Chunk.WATERLEVEL + 0.01f);
+	refractionClipPlane = new Vector4f(0, 0, -1, Chunk.WATERLEVEL + 0.01f);
+	renderClipPlane = new Vector4f(0, 0, -1, 10000);
+
+	reflection = new FrameBufferObject(REFLECTION_WIDTH, REFLECTION_HEIGHT, false);
+	refraction = new FrameBufferObject(REFRACTION_WIDTH, REFRACTION_HEIGHT, true);
+	
+
+	Detail.init();
+
+	walk = new Walk();
+	GraphicsManager.toggleFog();
+}
 
 	/**
 	 * Sets the seeds for everything
@@ -237,7 +238,7 @@ public class Window implements Runnable {
 		worldRandom.setSeed(119);// 119 : 5
 		mathRandom.setSeed(worldRandom.nextLong());
 		entityRandom.setSeed(0);
-		// World.perlinSeed = mathRandom.nextInt();
+		//World.perlinSeed = mathRandom.nextInt();
 	}
 
 	/**
@@ -285,56 +286,53 @@ public class Window implements Runnable {
 		// objectManager.render();
 
 		// move camera to appropriate location and render reflection texture
-	//	float camDist = GraphicsManager.camera.pos.z - Chunk.WATERLEVEL;
-	//	float targetDist = GraphicsManager.camera.getTarget().z - Chunk.WATERLEVEL;
+		float camDist = GraphicsManager.camera.pos.z - Chunk.WATERLEVEL;
+		float targetDist = GraphicsManager.camera.getTarget().z - Chunk.WATERLEVEL;
 
 		// GraphicsManager.camera.flipCamera();
-	//	GraphicsManager.camera.moveCamera(new Vector3f(0, 0, -camDist * 2));
-	//	GraphicsManager.camera.moveTarget(new Vector3f(0, 0, -targetDist * 2));
-	//	ShaderManager.setCamera(GraphicsManager.camera, GraphicsManager.dirLight);
+		GraphicsManager.camera.moveCamera(new Vector3f(0, 0, -camDist * 2));
+		GraphicsManager.camera.moveTarget(new Vector3f(0, 0, -targetDist * 2));
+		ShaderManager.setCamera(GraphicsManager.camera, GraphicsManager.dirLight);
 		// bind reflection buffer and render to it
-	//	reflection.activate();
-	//	glClearColor(CLEAR_COLOUR.x, CLEAR_COLOUR.y, CLEAR_COLOUR.z, 1.0f);
-	//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//	glEnable(GL_DEPTH_TEST);
-	//	world.renderLand(reflectionClipPlane);
-	//	objectManager.render(reflectionClipPlane);
-		//entityManager.render(reflectionClipPlane);
-		//lava.render(reflectionClipPlane);
+		reflection.activate();
+		glClearColor(CLEAR_COLOUR.x, CLEAR_COLOUR.y, CLEAR_COLOUR.z, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_DEPTH_TEST);
+		world.renderLand(reflectionClipPlane);
+		objectManager.render(reflectionClipPlane);
+		entityManager.render(reflectionClipPlane);
+		lava.render(reflectionClipPlane);
+		
 
-		// move camera back and render refraction texture
-		// GraphicsManager.camera.flipCamera();
+		//	move camera back and render refraction texture
+		//GraphicsManager.camera.flipCamera();
 
-		//GraphicsManager.camera.moveCamera(new Vector3f(0, 0, camDist * 2));
-		//GraphicsManager.camera.moveTarget(new Vector3f(0, 0, targetDist * 2));
-		//ShaderManager.setCamera(GraphicsManager.camera, GraphicsManager.dirLight);
+		GraphicsManager.camera.moveCamera(new Vector3f(0, 0, camDist * 2));
+		GraphicsManager.camera.moveTarget(new Vector3f(0, 0, targetDist * 2));
+		ShaderManager.setCamera(GraphicsManager.camera, GraphicsManager.dirLight);
 		// bind refraction buffer and render to it
-		//refraction.activate();
-		//glClearColor(CLEAR_COLOUR.x, CLEAR_COLOUR.y, CLEAR_COLOUR.z, 1.0f);
-		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		//glEnable(GL_DEPTH_TEST);
-		//world.renderLand(refractionClipPlane);
-		//objectManager.render(refractionClipPlane);
-		//entityManager.render(refractionClipPlane);
-		//lava.render(refractionClipPlane);
+		refraction.activate();
+		glClearColor(CLEAR_COLOUR.x, CLEAR_COLOUR.y, CLEAR_COLOUR.z, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_DEPTH_TEST);
+		world.renderLand(refractionClipPlane);
+		objectManager.render(refractionClipPlane);
+		entityManager.render(refractionClipPlane);
+		lava.render(refractionClipPlane);
 
 		// render to screen
 		glBindFramebuffer(GL_FRAMEBUFFER, 0); // back to default
 		glViewport(0, 0, windowWidth, windowHeight);
 		glClearColor(CLEAR_COLOUR.x, CLEAR_COLOUR.y, CLEAR_COLOUR.z, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		// world.renderLand(renderClipPlane);
-		// objectManager.render(renderClipPlane);
-		// water.render(renderClipPlane); // do NOT attempt to render water
-		// anywhere other than to screen
-		// entityManager.render(renderClipPlane);
-		// lava.render(renderClipPlane);
-		// trees.render(renderClipPlane);
-		// Detail.render(renderClipPlane);
-		
-		//tunnel.render(renderClipPlane);
-	
-		
+		world.renderLand(renderClipPlane);
+		objectManager.render(renderClipPlane);
+		water.render(renderClipPlane); // do NOT attempt to render water
+										// anywhere other than to screen
+		entityManager.render(renderClipPlane);
+		lava.render(renderClipPlane);
+		//trees.render(renderClipPlane);
+		Detail.render(renderClipPlane);
 		walk.render(renderClipPlane);
 	}
 
