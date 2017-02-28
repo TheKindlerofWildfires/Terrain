2f274bd128a2a1f6665fef0d871cd0ff5f4cc265
Radiance <radientross@gmail.com>
2017-02-28 15:37:18 -0600

    Thats a little better

--- src/entity/Slasher.java
+++ src/entity/Slasher.java
@@ -3,7 +3,6 @@ package entity;
 import graphics.Window;
 import maths.Vector3f;
 import object.GameObject;
-import physics.Time;
 
 public class Slasher extends Monster{
 	Vector3f direction = new Vector3f(0,0,0);
@@ -15,8 +14,8 @@ public class Slasher extends Monster{
 	public void construct(){
 		GameObject blade1 = new GameObject("resources/models/cyl.obj", "none", true);
 		GameObject blade2 = new GameObject("resources/models/cyl.obj", "none", true);
-		blade1.placeAt(-.75f, -1.5f, 0);
-		blade2.placeAt(-.75f, 1.5f, 0);
+		blade1.placeAt(0f, -1.5f, 0);
+		blade2.placeAt(-0f, 1.5f, 0);
 		blade1.scale(1f, .04f, 1f);
 		blade2.scale(1f, .04f, 1f);
 		blade1.rotate(90,1,0,0);
@@ -28,32 +27,32 @@ public class Slasher extends Monster{
 		satellites.stream().forEach(o -> o.material.colour = new Vector3f(0,1,1));
 	}
 	public void slash(int time){
-		double t = time*Math.PI/10;
-		satellites.stream().forEach(o -> o.velocity = (new Vector3f((float)Math.cos(t + Math.PI)*.2f, 0,0)));
+		satellites.stream().forEach(o -> o.translate(new Vector3f((time-30)*.004f,0,0)));
+		//satellites.stream().forEach(o-> o.rotate(90, 0, 1, 0));
 	}
 	
 	public void update(){
 		move();
 		satellites.stream().forEach(o -> o.physic());
 		physic(); 
-		if(slashing>0){
-			slashing--;
+		if(slashing>=0){
 			slash(slashing);
+			slashing--;
 		}
 	}
 	private void move() { 
 		int dir = Window.entityRandom.nextInt(10);
 		if( dir == 1){
-			direction = new Vector3f(0,1,0);
+			direction = new Vector3f(0,.001f,0);
 		}else if(dir == 2){
-			//direction = new Vector3f(0,-1,0);
+			direction = new Vector3f(0,-.001f,0);
 		}else if (dir ==3){
-			if(slashing<=0){
-			slashing = 10;}
+			if(slashing<0){
+			slashing = 60;}
 		}else{
 			direction = new Vector3f(0,0,0);
 		}
-		acceleration.add(direction);
+		//addAcceleration(direction);
 		
 	}
 
